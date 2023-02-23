43e8fb5cee04f8fd37d41a5f89c1f84480656292
ivantong97 <120369163+ivantong97@users.noreply.github.com>
2023-02-23 12:04:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+exporter = AzureMonitorTraceExporter(connection_string="InstrumentationKey=1482a39a-7e96-46a1-85fb-04733d09a03d;IngestionEndpoint=https://southeastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/")
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
 app = Flask(__name__)
 
 
@@ -18,6 +30,12 @@ def hello():
    name = request.form.get('name')
 
    if name:
+      try:
+          with tracer.start_as_current_span("Hello %s" % name) as span:
+              # This exception will be automatically recorded
+              raise Exception("Custom exception message.")
+      except Exception:
+          print("Exception raised") 
        print('Request for hello page received with name=%s' % name)
        return render_template('hello.html', name = name)
    else:
